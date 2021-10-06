a5382e3823cda20a90c9e0a36ae16dacba327283
Brijesh <brijesh1875@gmail.com>
2021-10-06 07:42:44 +0530

    Feat: Learned about  Router and implemented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import Header from "./components/Header"
-import Footer from "./components/Footer"
-import Content from "./components/Content"
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import Home from "./pages/Home";
+import About from "./pages/About";
+
 function App () {
      return <>
-          <Header/>
-          <Content/>
-          <Footer/>
+          
+          <Router>
+               <Link to='/' style = {{ marginRight : '20px', }}>Home</Link>
+               <Link to='/about' style = {{ marginRight : '20px', }}>About</Link>
+               <Switch>
+                    <Route path = '/' component = {Home} exact></Route>
+                    <Route path = '/about' component = {About} exact></Route>
+               </Switch>
+          </Router>
+
      </>
 }
 
